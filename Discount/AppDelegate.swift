bb43743b8496efbb34b892bb36763f32869484b6
Mark Cornelisse <info@markcornelisse.nl>
2018-01-31 17:32:35 +0100

    Initial Commit

---
+++ Discount/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Discount
+//
+//  Created by Mark Cornelisse on 31/01/2018.
+//  Copyright © 2018 Mark Cornelisse. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Discount/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Discount
+//
+//  Created by Mark Cornelisse on 31/01/2018.
+//  Copyright © 2018 Mark Cornelisse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
